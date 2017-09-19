cf1707b90af7194ae3ae1314c89fe34d7f84882d
bigtreezhudi <zhudi@tree.ai>
2017-09-19 15:49:10 +0800

    添加了click_start_stop_button

--- SimpleHomePageUi.py
+++ SimpleHomePageUi.py
@@ -4,28 +4,27 @@ from PyQt4 import QtCore, QtGui
 class UiHomePage(QtGui.QWidget):
     def __init__(self, parent=None):
         super(UiHomePage, self).__init__()
-        self.setupUi()
+        self.setup_ui()
         self.show()
         self.raise_()
 
-    def setupUi(self):
+    def setup_ui(self):
         # 设置窗口大小为最小
         self.showMinimized()
-        self.centralWidget = QtGui.QVBoxLayout()
+        self.central_widget = QtGui.QVBoxLayout()
 
         # 设置全局的layout
-        self.setLayout(self.centralWidget)
+        self.setLayout(self.central_widget)
 
         #　添加一个显示日志的ListView
-        self.logHBoxLayout = QtGui.QHBoxLayout()
-        self.loglistView = QtGui.QListView()
-        self.logHBoxLayout.addWidget(self.loglistView)
-        self.loglistView.setSpacing(4)
-        self.centralWidget.addLayout(self.logHBoxLayout)
-        # self.listView.clicked.connect(self.on_listView_cliked)
+        self.log_HBoxLayout = QtGui.QHBoxLayout()
+        self.log_ListView = QtGui.QListView()
+        self.log_HBoxLayout.addWidget(self.log_ListView)
+        self.log_ListView.setSpacing(4)
+        self.central_widget.addLayout(self.log_HBoxLayout)
 
         #　添加进度条
-        self.progressBarHBoxLayout = QtGui.QHBoxLayout()
+        self.progressBar_HBoxLayout = QtGui.QHBoxLayout()
         self.progressBar = QtGui.QProgressBar()
         self.progressBar.setProperty("value", 0)
         style = """
@@ -40,17 +39,24 @@ class UiHomePage(QtGui.QWidget):
                     width: 20px;
                 }"""
         self.progressBar.setStyleSheet(style)
-        self.progressBarHBoxLayout.addWidget(self.progressBar)
-        self.centralWidget.addLayout(self.progressBarHBoxLayout)
+        self.progressBar_HBoxLayout.addWidget(self.progressBar)
+        self.central_widget.addLayout(self.progressBar_HBoxLayout)
 
         #　添加Start和Cancel按钮
-        self.StartAndCancelHBoxLayout = QtGui.QHBoxLayout()
-        self.Button_Cancel = QtGui.QPushButton("Start")
-        self.StartAndCancelHBoxLayout.addWidget(self.Button_Cancel)
-        self.Button_Display = QtGui.QPushButton("Cancel")
-        self.StartAndCancelHBoxLayout.addWidget(self.Button_Display)
-        # self.Button_Display.clicked.connect(self.Display)
-        self.centralWidget.addLayout(self.StartAndCancelHBoxLayout)
+        self.start_cancel_HBoxLayout = QtGui.QHBoxLayout()
+        self.start_stop_button = QtGui.QPushButton("Start")
+        self.start_cancel_HBoxLayout.addWidget(self.start_stop_button)
+        self.start_stop_button.clicked.connect(self.click_start_stop_button)
+        self.cancel_button = QtGui.QPushButton("Cancel")
+        self.start_cancel_HBoxLayout.addWidget(self.cancel_button)
+        self.central_widget.addLayout(self.start_cancel_HBoxLayout)
+
+    def click_start_stop_button(self):
+        currentName = self.start_stop_button.text()
+        if currentName == "Start":
+            self.start_stop_button.setText("Finish")
+        else:
+            self.start_stop_button.setText("Start")
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
