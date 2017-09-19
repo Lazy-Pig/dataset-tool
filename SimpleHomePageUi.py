4234a88368c0d7801c9637773274cb4a970c8882
bigtreezhudi <zhudi@tree.ai>
2017-09-19 15:27:04 +0800

    简化了控件摆放的方式

--- SimpleHomePageUi.py
+++ SimpleHomePageUi.py
@@ -16,31 +16,17 @@ class UiHomePage(QtGui.QWidget):
         # 设置全局的layout
         self.setLayout(self.centralWidget)
 
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
-        self.centralWidget.addLayout(self.verticalLayout_2)
-        self.horizontalLayout_6 = QtGui.QHBoxLayout()
-        self.lineEdit_IPFile = QtGui.QLineEdit()
-        self.centralWidget.addWidget(self.lineEdit_IPFile)
-        self.horizontalLayout_6.addWidget(self.lineEdit_IPFile)
-        self.Button_Open = QtGui.QPushButton("Open")
-        self.centralWidget.addWidget(self.Button_Open)
-        self.horizontalLayout_6.addWidget(self.Button_Open)
-        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
-
-        self.horizontalLayout_4 = QtGui.QHBoxLayout()
-
-        self.listView = QtGui.QListView()
-        self.centralWidget.addWidget(self.listView)
-        self.horizontalLayout_4.addWidget(self.listView)
-        self.listView.setSpacing(4)
+        #　添加一个显示日志的ListView
+        self.logHBoxLayout = QtGui.QHBoxLayout()
+        self.loglistView = QtGui.QListView()
+        self.logHBoxLayout.addWidget(self.loglistView)
+        self.loglistView.setSpacing(4)
+        self.centralWidget.addLayout(self.logHBoxLayout)
         # self.listView.clicked.connect(self.on_listView_cliked)
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
-        self.horizontalLayout_3 = QtGui.QHBoxLayout()
-
-        # add the progressbar
+        #　添加进度条
+        self.progressBarHBoxLayout = QtGui.QHBoxLayout()
         self.progressBar = QtGui.QProgressBar()
-        self.centralWidget.addWidget(self.progressBar)
         self.progressBar.setProperty("value", 0)
         style = """
                 QProgressBar {
@@ -54,36 +40,17 @@ class UiHomePage(QtGui.QWidget):
                     width: 20px;
                 }"""
         self.progressBar.setStyleSheet(style)
-        ###############################################################
+        self.progressBarHBoxLayout.addWidget(self.progressBar)
+        self.centralWidget.addLayout(self.progressBarHBoxLayout)
 
-        self.Button_Plus = QtGui.QPushButton("+")
-        # self.Button_Plus.clicked.connect(self.Click_Plus)
-        self.Button_Minus = QtGui.QPushButton("-")
-        # self.Button_Minus.clicked.connect(self.Clik_Minus)
-        self.PMHbox = QtGui.QHBoxLayout()
-        self.PMHbox.addWidget(self.Button_Plus)
-        self.PMHbox.addWidget(self.Button_Minus)
-        self.PMHbox.addStretch(1)
-
-        self.PMVbox = QtGui.QVBoxLayout()
-        self.PMVbox.addLayout(self.PMHbox)
-        self.horizontalLayout_3.addWidget(self.progressBar)
-
-        self.verticalLayout_2.addLayout(self.PMVbox)
-        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.horizontalLayout = QtGui.QHBoxLayout()
-
+        #　添加Start和Cancel按钮
+        self.StartAndCancelHBoxLayout = QtGui.QHBoxLayout()
         self.Button_Cancel = QtGui.QPushButton("Start")
-        self.centralWidget.addWidget(self.Button_Cancel)
-        self.horizontalLayout.addWidget(self.Button_Cancel)
-
+        self.StartAndCancelHBoxLayout.addWidget(self.Button_Cancel)
         self.Button_Display = QtGui.QPushButton("Cancel")
-        self.centralWidget.addWidget(self.Button_Display)
-        self.horizontalLayout.addWidget(self.Button_Display)
+        self.StartAndCancelHBoxLayout.addWidget(self.Button_Display)
         # self.Button_Display.clicked.connect(self.Display)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.centralWidget.addLayout(self.StartAndCancelHBoxLayout)
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
