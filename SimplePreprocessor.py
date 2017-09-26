1ef7aeb63b52dee9f21373315e510b51cfba911b
bigtreezhudi <zhudi@tree.ai>
2017-09-26 16:12:37 +0800

    将每个动作产生的每个数据流分成三个序列的功能开发好了

---
+++ SimplePreprocessor.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+import os
+import logging
+import pickle
+import re
+from scapy.all import PcapReader, wrpcap, Packet, NoPayload
+
+HOSTIP = '10.42.0.166'
+
+class SimplePreprocessor(object):
+    """
+    实现了Analyzing Android Encrypted Network Traffic to Identify User Actions中的预处理
+    """
+    def __init__(self, dataset_dir="dataset", save_ips_path='weibo_out/save_ips.pkl'):
+        self.dataset_path = dataset_dir
+        self.save_ips_path = save_ips_path
+        self._load_ips()
+
+    def _load_ips(self):
+        """
+        过滤留下的ip加载进来
+        """
+        with open(self.save_ips_path, 'r') as f:
+            self.save_ips = pickle.load(f)
+
+    def represent_by_length(self):
+        """
+        将数据集中是的每一条样本用收到或发送的数据包长度表示
+        """
+        if not os.path.isdir(self.dataset_path):
+            logging.info("数据集目录　%s　不存在" % self.dataset_path)
+            return
+
+        for pcap_file in os.listdir(self.dataset_path):
+            if not os.path.isfile(pcap_file):
+                pass
+            # 从文件名中匹配出标签，匹配从"_"到"."之间的字符串
+            m = re.match(r"^.*\_(.*)\..*$" , pcap_file)
+            label = m.group(1)
+            port2tuple = {}
+            with PcapReader(self.dataset_path + "/" + pcap_file) as pcap_reader:
+                for packet in pcap_reader:
+                    # 遍历当前pcap文件中的每一个packet
+                    try:
+                        src = packet['IP'].fields['src']
+                        dst = packet['IP'].fields['dst']
+                        len = packet['IP'].fields['len']
+                    except:
+                        continue
+                    try:
+                        sport = packet['TCP'].fields['sport']
+                        dport = packet['TCP'].fields['dport']
+                    except:
+                        continue
+                    if (src in self.save_ips) or (dst in self.save_ips):
+                        # 以手机的端口号来区分一个动作所产生的不同流，
+                        # 每个流中的手机发送出的IP包长度为正，手机接收到IP包长度为负
+                        port, tag = (sport, 1) if src == HOSTIP else (dport, -1)
+                        # port2tuple的键是端口号，值是一个含有３个list的tuple，
+                        # 第一个是发送和接收到数据包长度，第二个是发送的数据包长度，第三个是接收的数据包长度
+                        if port in port2tuple:
+                            port2tuple[port][0].append(tag * len)
+                        else:
+                            port2tuple[port] = ([tag * len], [], [])
+                        if tag == 1:
+                            port2tuple[port][1].append(len)
+                        elif tag == -1:
+                            port2tuple[port][2].append(len)
+                        else:
+                            raise Exception("既不是发送出去的包也不是接收到的包")
+            print label, port2tuple.values()
+
+                        # logging.info("[saved]source %s:%s, destination %s:%s", src, sport, dst, dport)
+                    # else:
+                    #     logging.info("[droped]source %s:%s, destination %s:%s", src, sport, dst, dport)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d]%(message)s',
+                        datefmt='%m-%d %H:%M')
+    preprocessor = SimplePreprocessor()
+    preprocessor.represent_by_length()
